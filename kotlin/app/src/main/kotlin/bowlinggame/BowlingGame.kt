e30bdc6f9e8a9e6be63819d124196b711051514a
Douglas de Oliveira Mendes <douglas.mendes@gmail.com>
2021-10-03 10:52:26 -0300

    Solved kotlin

---
+++ kotlin/app/src/main/kotlin/bowlinggame/BowlingGame.kt
@@ -0,0 +1,69 @@
+package bowlinggame
+
+import java.util.*
+
+class BowlingGame {
+    private var totalScore: Int = 0
+    private var previousPins: Int = -1
+    private var currentPins: Int = -1
+    private var rollsToDouble: LinkedList<Int> = LinkedList()
+    private var firstAttemptWithinFrame: Boolean = true
+    private var frame: Int = 1
+
+    fun roll(pins: Int) {
+        currentPins = pins
+        processBonus()
+        totalScore += currentPins
+        prepareBonus()
+    }
+
+    private fun processBonus() {
+        if (rollsToDouble.isNotEmpty()) {
+            val bonus = rollsToDouble.removeFirst()
+            totalScore += bonus * currentPins
+        }
+    }
+
+    private fun prepareBonus() {
+        if (frame != 10) {
+            if (strike()) {
+                prepareStrikeBonus()
+                frame++
+            } else {
+                if (spare()) {
+                    prepareSpareBonus()
+                }
+                if (!firstAttemptWithinFrame) {
+                    frame++
+                }
+                firstAttemptWithinFrame = !firstAttemptWithinFrame
+                previousPins = currentPins
+            }
+        }
+    }
+
+    private fun strike() = firstAttemptWithinFrame && currentPins == 10
+
+    private fun spare() = !firstAttemptWithinFrame && previousPins + currentPins == 10
+
+    private fun prepareStrikeBonus() {
+        prepareSpareBonus()
+        prepareSecondRollBonus()
+    }
+
+    private fun prepareSpareBonus() {
+        if (rollsToDouble.isEmpty()) {
+            prepareSecondRollBonus()
+        } else {
+            rollsToDouble[0]++
+        }
+    }
+
+    private fun prepareSecondRollBonus() {
+        rollsToDouble += 1
+    }
+
+    fun score() : Int {
+        return totalScore
+    }
+}
---
+++ kotlin/app/src/test/kotlin/bowlinggame/BowlingGameTest.kt
@@ -0,0 +1,195 @@
+package bowlinggame
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class BowlingGameTest {
+
+    private var game: BowlingGame = BowlingGame()
+
+    @Before
+    fun setUp() {
+        game = BowlingGame()
+    }
+
+    @Test
+    fun bowlingAllGuttersReturnsZeroScore() {
+        // Arrange
+        val expected = 0
+
+        // Act
+        game.roll(0)
+        val actual = game.score()
+
+        // Assert
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun singlePins() {
+        // Arrange
+        val expected = 10
+
+        // Act
+        repeat(10) { game.roll(1) }
+        val actual = game.score()
+
+        // Assert
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun singleSpare() {
+        // Arrange
+        val expected = 18
+
+        // Act
+        game.roll(9)
+        game.roll(1)
+        game.roll(4)
+        val actual = game.score()
+
+        // Assert
+        Assert.assertEquals(expected, actual)
+    }
+
+
+    @Test
+    fun singleStrike() {
+        // Arrange
+        val expected = 16
+
+        // Act
+        game.roll(10)
+        game.roll(1)
+        game.roll(2)
+        val actual = game.score()
+
+        // Assert
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun perfectGame() {
+        // Arrange
+        val expected = 300
+
+        // Act
+        repeat(12) { game.roll(10) }
+        val actual = game.score()
+
+        // Assert
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun dutch200Even() {
+        // Arrange
+        val expected = 200
+
+        // Act
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(10)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(10)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(10)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(10)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(10)
+        game.roll(9)
+        game.roll(1)
+        val actual = game.score()
+
+        // Assert
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun dutch200Odd() {
+        // Arrange
+        val expected = 200
+
+        // Act
+        game.roll(10)
+        game.roll(9)
+        game.roll(1)
+        game.roll(10)
+        game.roll(9)
+        game.roll(1)
+        game.roll(10)
+        game.roll(9)
+        game.roll(1)
+        game.roll(10)
+        game.roll(9)
+        game.roll(1)
+        game.roll(10)
+
+        game.roll(9)
+        game.roll(1)
+        game.roll(10)
+        val actual = game.score()
+
+        // Assert
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun nineSpareGame() {
+        // Arrange
+        val expected = 190
+
+        // Act
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(9)
+        game.roll(1)
+
+        game.roll(9)
+        game.roll(1)
+        game.roll(9)
+
+        val actual = game.score()
+
+        // Assert
+        Assert.assertEquals(expected, actual)
+    }
+}
