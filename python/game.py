55f8c958026ab9bdab163941f584b82530a06346
Douglas de Oliveira Mendes <douglas.mendes@gmail.com>
2021-10-03 11:25:11 -0300

    Solved some test cases for python version

--- python/game.py
+++ python/game.py
@@ -1,9 +1,50 @@
 class Game(object):
     def __init__(self):
-        print("new game!")
+        self.total_score = 0
+        self.current_roll = -1
+        self.previous_roll = -1
+        self.frame = 1
+        self.first_attempt_within_frame = True
+        self.bonus = []
 
     def roll(self, pins):
-        print(f'rolling {pins}')
+        self.current_roll = pins
+        self.process_current_roll()
+        self.prepare_bonus()
+
+    def process_current_roll(self):
+        self.process_bonus()
+        self.total_score += self.current_roll
+
+    def process_bonus(self):
+        if self.bonus:
+            self.total_score += self.bonus.pop(0) * self.current_roll
+
+    def prepare_bonus(self):
+        if self.frame != 10:
+            if self.first_attempt_within_frame:
+                self.frame += 1
+                if self.current_roll == 10:
+                    self.prepare_strike_bonus()
+                else:
+                    self.possible_spare_next_roll()
+            else:
+                if self.previous_roll + self.current_roll == 10:
+                    self.prepare_spare_bonus()
+
+    def possible_spare_next_roll(self):
+        self.previous_roll = self.current_roll
+        self.first_attempt_within_frame = not self.first_attempt_within_frame
+
+    def prepare_spare_bonus(self):
+        self.bonus.append(1)
+
+    def prepare_strike_bonus(self):
+        if self.bonus:
+            self.bonus[0] += 1
+        else:
+            self.bonus.append(1)
+        self.bonus.append(1)
 
     def score(self):
-        return -1
+        return self.total_score
--- python/test.py
+++ python/test.py
@@ -12,13 +12,20 @@ class TestGame(unittest.TestCase):
         self.assertEqual(self.g.score(), 1)
 
     def test_gutter_game(self):
-        self.fail("Not yet implemented")
+        for _ in range(10):
+            self.g.roll(0)
+        self.assertEqual(self.g.score(), 0)
 
     def test_all_ones(self):
-        self.fail("Not yet implemented")
+        for _ in range(10):
+            self.g.roll(1)
+        self.assertEqual(self.g.score(), 10)
 
     def test_one_spare(self):
-        self.fail("Not yet implemented")
+        self.g.roll(1)
+        self.g.roll(9)
+        self.g.roll(1)
+        self.assertEqual(self.g.score(), 12)
 
     def test_one_strike(self):
         self.g.roll(10)
@@ -27,34 +34,36 @@ class TestGame(unittest.TestCase):
         self.assertEqual(self.g.score(), 10 + 2*2 + 2*1)
 
     def test_perfect_game(self):
-        self.fail("Not yet implemented")
+        for _ in range(12):
+            self.g.roll(10)
+        self.assertEqual(self.g.score(), 300)
 
-    def test_roll_of_greater_than_10_raises_error(self):
-        self.fail("Not yet implemented")
-
-    def test_negative_roll_raises_error(self):
-        self.fail("Not yet implemented")
-
-    def test_frame_rolls_in_sum_greater_than_10_raises_error(self):
-        self.fail("Not yet implemented")
-
-    def test_more_than_21_rolls_raises_error(self):
-        self.fail("Not yet implemented")
-
-    def test_a_roll_after_2_non_bonus_rolls_in_10th_frame_raises_error(self):
-        self.fail("Not yet implemented")
-
-    def test_getting_score_with_11_strikes_raises_error(self):
-        self.fail("Not yet implemented")
-
-    def test_getting_score_with_10_spares_and_missed_bonus_raises_error(self):
-        self.fail("Not yet implemented")
-
-    def test_getting_score_with_19_ones_raises_error(self):
-        self.fail("Not yet implemented")
-
-    def test_getting_score_with_4_ones_raises_error(self):
-        self.fail("Not yet implemented")
+    # def test_roll_of_greater_than_10_raises_error(self):
+    #     self.fail("Not yet implemented")
+    #
+    # def test_negative_roll_raises_error(self):
+    #     self.fail("Not yet implemented")
+    #
+    # def test_frame_rolls_in_sum_greater_than_10_raises_error(self):
+    #     self.fail("Not yet implemented")
+    #
+    # def test_more_than_21_rolls_raises_error(self):
+    #     self.fail("Not yet implemented")
+    #
+    # def test_a_roll_after_2_non_bonus_rolls_in_10th_frame_raises_error(self):
+    #     self.fail("Not yet implemented")
+    #
+    # def test_getting_score_with_11_strikes_raises_error(self):
+    #     self.fail("Not yet implemented")
+    #
+    # def test_getting_score_with_10_spares_and_missed_bonus_raises_error(self):
+    #     self.fail("Not yet implemented")
+    #
+    # def test_getting_score_with_19_ones_raises_error(self):
+    #     self.fail("Not yet implemented")
+    #
+    # def test_getting_score_with_4_ones_raises_error(self):
+    #     self.fail("Not yet implemented")
 
 
 if __name__ == '__main__':
