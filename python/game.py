b4b5e5e6cc548147e3fe2fc8b63240d88cd077c1
Douglas de Oliveira Mendes <douglas.mendes@gmail.com>
2021-10-04 10:17:01 -0300

    Refactoring: extracted some methods.

--- python/game.py
+++ python/game.py
@@ -8,11 +8,14 @@ class Game(object):
         self.bonus = []
 
     def roll(self, pins):
-        if self.first_attempt_within_frame and self.frame < 10:
-            self.frame += 1
+        self.increment_frame_count_if_new()
         self.current_roll = pins
         self.process_current_roll()
         self.prepare_bonus()
+
+    def increment_frame_count_if_new(self):
+        if self.first_attempt_within_frame and self.frame < 10:
+            self.frame += 1
 
     def process_current_roll(self):
         self.process_bonus()
@@ -23,16 +26,25 @@ class Game(object):
             self.total_score += self.bonus.pop(0) * self.current_roll
 
     def prepare_bonus(self):
-        if self.frame != 10:
+        if not self.last_frame():
             if self.first_attempt_within_frame:
-                if self.current_roll == 10:
+                if self.strike():
                     self.prepare_strike_bonus()
                 else:
                     self.possible_spare_next_roll()
             else:
-                if self.previous_roll + self.current_roll == 10:
+                if self.spare():
                     self.prepare_spare_bonus()
                 self.first_attempt_within_frame = not self.first_attempt_within_frame
+
+    def last_frame(self):
+        return self.frame == 10
+
+    def strike(self):
+        return self.current_roll == 10
+
+    def spare(self):
+        return self.previous_roll + self.current_roll == 10
 
     def possible_spare_next_roll(self):
         self.previous_roll = self.current_roll
