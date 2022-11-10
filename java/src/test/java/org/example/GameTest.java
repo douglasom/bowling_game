ec5d2f02f64ca9510447ca2480aa204cb994bf51
Douglas de Oliveira Mendes <douglas.mendes@gmail.com>
2022-11-10 09:07:09 -0300

    Added java skeleton game.

---
+++ java/src/test/java/org/example/GameTest.java
@@ -0,0 +1,63 @@
+
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+    private Game game;
+
+    @BeforeEach
+    public void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    void singlePin() {
+        game.roll(3);
+        assertEquals(3, game.score());
+    }
+
+//    @Test
+//    void gutterGame() {
+//        fail("Not implemented");
+//    }
+//
+//    @Test
+//    void allOnes() {
+//        fail("Not implemented");
+//    }
+//
+//    @Test
+//    void singleSpare() {
+//        fail("Not implemented");
+//    }
+//
+//    @Test
+//    void singleStrike() {
+//        fail("Not implemented");
+//    }
+//
+//    @Test
+//    void perfectGame() {
+//        fail("Not implemented");
+//    }
+//
+//    @Test
+//    void dutch200Even() {
+//        fail("Not implemented");
+//    }
+//
+//    @Test
+//    void dutch200Odd() {
+//        fail("Not implemented");
+//    }
+//
+//    @Test
+//    void nineSpareGame() {
+//        fail("Not implemented");
+//    }
+
+}
